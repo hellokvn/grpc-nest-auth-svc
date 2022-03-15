1a8b2407036ea6da6121ce81c5e9d3c9fdfd74c1
Kevin <hellokevinvogel@gmail.com>
2022-03-15 08:02:17 +0100

    chore(): add typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'micro_auth',
+      username: 'kevin',
+      password: null,
+      entities: ['dist/**/*.entity.{ts,js}'],
+      synchronize: true, // never true in production!
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
